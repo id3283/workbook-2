d1f15232246533f55f7be6526af8a4d1e4bce151
dave <dave@brightworkdigital.com>
2025-04-21 21:24:17 +0100

    added try/catch example

--- in-class/src/main/java/com/pluralsight/Main.java
+++ in-class/src/main/java/com/pluralsight/Main.java
@@ -2,23 +2,94 @@ package com.pluralsight;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+import java.util.Scanner;
 import java.util.regex.Pattern;
 
 class Main {
-
     public static void main(String[] args) {
-        loopsAndArrays();
+        intentionalErrorsNoReallyIntentional();
+    }
+
+    static void intentionalErrorsNoReallyIntentional() {
+        String[] names =  {"Kevin", "Mahlet"};
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter an integer (nothing else... it's important!)");
+
+        int i = 0;
+        try {
+            i = scanner.nextInt();
+        } catch (Exception e) {
+            System.out.println("That wasn't an integer, I said it was important!");
+            e.printStackTrace();
+        }
+
+        System.out.println("Here is the integer: " + i);
+
+
+//        } catch (Exception e) {
+//            System.out.println("Here's the exception: " + e);
+//            System.out.println("Meh, whatever... I'm just going to carry on.");
+//        }
+
+    }
+
+    static void parseProblem() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter integer to be scanned with nextInt: ");
+//        int firstInt = scanner.nextInt();
+        String userInput = scanner.nextLine();
+        int firstInt = Integer.parseInt(userInput);
+
+        System.out.println("Enter a string (or try to!): ");
+        String inputString = scanner.nextLine();
+
+        System.out.println("You entered " + firstInt + " " + "followed by: " + inputString);
+    }
+
+    static void errorMethod() {
+
+        int[] ageArray = {56, 22, 33};
+        for (int i = 0; i < 57; i++) {
+            System.out.println(ageArray[i]);
+
+        }
     }
 
     static void loopsAndArrays() {
-        int[] integerArray = new int[5];
-        String[] stringArray = {"Izel", "Victoria", "Noah"};
+        int[] ageArray = {56, 22, 33};
+        int[] secondArray;
+//        secondArray = nameArray;
+        secondArray = myCopy(ageArray);
 
-        for (int i = 0; i < 3; i++) {
-            System.out.println("string at " + i + ": " + stringArray[i]);
+        int[] thirdArray = new int[ageArray.length];
+        System.arraycopy(ageArray, 0, thirdArray, 0, ageArray.length);
+
+//        String karyna = "Karyna";
+//        printName(karyna);
+
+//        for (int i = 0; i < nameArray.length; i++) {
+//            System.out.println("string at " + i + ": " + nameArray[i]);
+//        }
+//
+//        for (String name: nameArray) {
+//            System.out.println("Here is the name: " + name);
+//        }
+    }
+
+    public static int[] myCopy(int[] og) {
+        int[] newG = new int[og.length];
+
+        for (int i = 0; i < og.length; i++) {
+            newG[i] = og[i];
         }
 
+        return newG;
+    }
 
+    public static void printName(String x) {
+        System.out.println("name: " + x);
     }
 
     static void stringBuilder() {
